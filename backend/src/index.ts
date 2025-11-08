732e0fed166c24cf918bf9ca097b0fa17cd95bba
OHshajim <ajshajimmax@gmail.com>
2025-11-08 13:25:26 +0600

    final submission

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,7 +6,7 @@ import expenseRoutes from "./routes/expense";
 
 dotenv.config();
 const app = express();
-app.use(cors());
+app.use(cors({origin: "*"}));
 app.use(express.json());
 
 connectDB();
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getDeviceId } from "../utils/device";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://developer-look-back.vercel.app/api",
     headers: {
         "Content-Type": "application/json",
         "X-Device-ID": getDeviceId(), // custom header
